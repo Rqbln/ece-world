6c59c6b952a8983cbc0b4333d7ae6dec28a0eb7a
Rqbln <118724817+Rqbln@users.noreply.github.com>
2023-05-10 22:37:44 +0200

    Fix2 problème .h

--- Menu/menu.h
+++ Menu/menu.h
@@ -8,8 +8,7 @@
 #endif //ECE_WORLD_MENU_H
 #include <allegro.h>
 #include "stdio.h"
-//#include "../Games/Course_Chevaux/chevaux.h"
-//#include "../Games/Jackpot/jackpot.h"
+
 
 
 #define HEIGHT 1080
