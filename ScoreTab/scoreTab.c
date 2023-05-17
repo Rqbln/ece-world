aa1e79ac1e1c3d5d9bb0c7c9a372284f244c2c82
baptiste321 <baptiste.chesnot@gmail.com>
2023-05-17 12:29:57 +0200

    ATTENTION modif Cmake

---
+++ ScoreTab/scoreTab.c
@@ -0,0 +1,5 @@
+//
+// Created by bapti on 17/05/2023.
+//
+
+#include "scoreTab.h"
---
+++ ScoreTab/scoreTab.h
@@ -0,0 +1,8 @@
+//
+// Created by bapti on 17/05/2023.
+//
+
+#ifndef ECE_WORLD_SCORETAB_H
+#define ECE_WORLD_SCORETAB_H
+
+#endif //ECE_WORLD_SCORETAB_H
